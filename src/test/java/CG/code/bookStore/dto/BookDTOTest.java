7baaef45eb2f3c067c6e784e99b46c4ae3e7a205
Noushad <basila.noushad@capgemini.com>
2023-11-08 16:02:15 +0530

    added all files in books

---
+++ src/test/java/CG/code/bookStore/dto/BookDTOTest.java
@@ -0,0 +1,24 @@
+package CG.code.bookStore.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookDTOTest {
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void builder() {
+    }
+}
